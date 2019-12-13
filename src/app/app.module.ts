019bc13f99ad4d8dcfbe92e05ec224d821c7678d
matthigg <mhh129@gmail.com>
2019-12-13 12:01:26 -0500

    Import ChannelsModule and ChannelsRoutingModule in AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AuthModule } from './auth/auth.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { ChannelsModule } from './channels/channels.module';
+import { ChannelsRoutingModule } from './channels/channels-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
@@ -22,7 +24,6 @@ import {
 
 // Components
 import { AppComponent } from './app.component';
-import { ChannelsComponent } from './channels/channels.component';
 import { NavToolbarComponent } from './nav-toolbar/nav-toolbar.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
 
@@ -32,7 +33,6 @@ import { ConsoleLogPipe } from './shared/console-log.pipe';
 @NgModule({
   declarations: [
     AppComponent,
-    ChannelsComponent,
     ConsoleLogPipe,
     NavToolbarComponent,
     SideNavComponent,
@@ -42,6 +42,8 @@ import { ConsoleLogPipe } from './shared/console-log.pipe';
     AuthModule,
     BrowserAnimationsModule,
     BrowserModule,
+    ChannelsModule,
+    ChannelsRoutingModule,
     HttpClientModule,
     MatButtonModule,
     MatCardModule,
