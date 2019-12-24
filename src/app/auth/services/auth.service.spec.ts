4383a1d9b48a70d30d840b6c63cb8ad5ec789265
matthigg <mhh129@gmail.com>
2019-12-24 13:34:23 -0500

    Update tests & code coverage

--- src/app/auth/services/auth.service.spec.ts
+++ src/app/auth/services/auth.service.spec.ts
@@ -51,4 +51,40 @@ describe('AuthService', () => {
       );
   });
 
+  it(`should handle error response 'EMAIL_NOT_FOUND'`, () => {
+    errorResponse = { error: { error: { message: 'EMAIL_NOT_FOUND' } } };
+    service['handleError'](errorResponse)
+      .subscribe(
+        response => expect(response).toBeFalsy(),
+        error => expect(error).toEqual('Email not found.'),
+      );
+  });
+
+  it(`should handle error response 'INVALID_EMAIL`, () => {
+    errorResponse = { error: { error: { message: 'INVALID_EMAIL' } } };
+    service['handleError'](errorResponse)
+      .subscribe(
+        response => expect(response).toBeFalsy(),
+        error => expect(error).toEqual('Email is invalid.'),
+      );
+  });
+
+  it(`should handle error response 'INVALID_PASSWORD`, () => {
+    errorResponse = { error: { error: { message: 'INVALID_PASSWORD' } } };
+    service['handleError'](errorResponse)
+      .subscribe(
+        response => expect(response).toBeFalsy(),
+        error => expect(error).toEqual('Password is invalid.'),
+      );
+  });
+
+  it(`should handle error response 'USER_DISABLED`, () => {
+    errorResponse = { error: { error: { message: 'USER_DISABLED' } } };
+    service['handleError'](errorResponse)
+      .subscribe(
+        response => expect(response).toBeFalsy(),
+        error => expect(error).toEqual('This account has been disabled by an administrator.')
+      );
+  });
+
 });
