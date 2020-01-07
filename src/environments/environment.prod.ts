a4e43d7641f9dbf7042648e3c9160c6192e2b48c
matthigg <mhh129@gmail.com>
2020-01-07 14:28:08 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAAC4JQbA0KOAL5RVMPyAIpp5XxWdnwRy8",
+    authDomain: "angular-chatroom-78cb6.firebaseapp.com",
+    databaseURL: "https://angular-chatroom-78cb6.firebaseio.com",
+    projectId: "angular-chatroom-78cb6",
+    storageBucket: "angular-chatroom-78cb6.appspot.com",
+    messagingSenderId: "8774604506",
+    appId: "1:8774604506:web:522ffb9230adc8415537e3"
+  }
 };
