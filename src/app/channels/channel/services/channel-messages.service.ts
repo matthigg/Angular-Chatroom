7e2bb43265e81431d0c47bd259b72300c0bcf7d5
matthigg <mhh129@gmail.com>
2020-01-07 14:14:37 -0500

    Firestore now stores array of chat messages

--- src/app/channels/channel/services/channel-messages.service.ts
+++ src/app/channels/channel/services/channel-messages.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 
 // Firestore
 import { AngularFirestore } from '@angular/fire/firestore';
+import * as firebase from 'firebase';
 
 @Injectable({
   providedIn: 'root'
@@ -12,15 +13,15 @@ export class ChannelMessagesService {
 
   // POST chatroom messages to Firestore
   addANewMessage(userName: string, channelName: string, message: string) {
-    return this.firestore.firestore.collection('channels').doc(channelName).set(
+    return this.firestore.firestore.collection('channels').doc(channelName).update(
       {
-        messages: [
+        messages: firebase.firestore.FieldValue.arrayUnion(
           {
             user: userName,
             time: new Date(),
             message: message,
           }
-        ]
+        )
       }
     )
   }
