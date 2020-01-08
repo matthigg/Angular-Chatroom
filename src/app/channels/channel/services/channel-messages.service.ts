c48905c0aa1288e198152c0606db2e2a9f0163f1
matthigg <mhh129@gmail.com>
2020-01-08 08:31:02 -0500

    Reduce size of firebase imports

--- src/app/channels/channel/services/channel-messages.service.ts
+++ src/app/channels/channel/services/channel-messages.service.ts
@@ -5,7 +5,9 @@ import { BehaviorSubject } from 'rxjs';
 
 // Firestore
 import { AngularFirestore } from '@angular/fire/firestore';
-import * as firebase from 'firebase';
+// import * as firebase from 'firebase/firestore';
+import firebase from '@firebase/app';
+import '@firebase/firestore';
 
 @Injectable({
   providedIn: 'root'
