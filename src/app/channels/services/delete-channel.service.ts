02581bb81e09db9dd41df0f0b988ba52f09f414b
matthigg <mhh129@gmail.com>
2020-01-27 09:01:43 -0500

    Update onDeleteChannel() method in DeleteChannelService to remove users[] from the users subcollection in Firestore when deleting a channel

--- src/app/channels/services/delete-channel.service.ts
+++ src/app/channels/services/delete-channel.service.ts
@@ -16,29 +16,35 @@ export class DeleteChannelService {
     channelCreator: string,
   ): Promise<any> {
 
-    // If a channel has nested Firestore credentials, ie. the channel is private,
-    // then delete the credentials before deleting the channel itself.
-    if (channelPermission === 'private') {
-      return this.onDeleteChannelCredentials(channelName)
-        .then(response => {
-          return this.firestore
-            .collection('channels')
-            .doc(channelName)
-            .delete();
-        })
-        .catch(error => console.log('=== Error:', error));
+    // Delete a channel's users list first
+    return this.onDeleteChannelUsers(channelName)
+      .then(response => {
 
-    // Delete a public channel
-    } else if (channelPermission === 'public') {
-      return this.firestore
-        .collection('channels')
-        .doc(channelName)
-        .delete();
-    } else {
-      return new Promise((resolve, reject) => {
-        reject('=== Error: channel permission is neither public nor private.')
-      });
-    }
+      // If a channel has nested Firestore credentials, ie. the channel is private,
+      // then delete the credentials before deleting the channel itself.
+      if (channelPermission === 'private') {
+        return this.onDeleteChannelCredentials(channelName)
+          .then(response => {
+            return this.firestore
+              .collection('channels')
+              .doc(channelName)
+              .delete();
+          })
+          .catch(error => console.log('=== Error:', error));
+
+      // Delete a public channel
+      } else if (channelPermission === 'public') {
+        return this.firestore
+          .collection('channels')
+          .doc(channelName)
+          .delete();
+      } else {
+        return new Promise((resolve, reject) => {
+          reject('=== Error: channel permission is neither public nor private.')
+        });
+      }
+    })
+    .catch(error => console.log('=== Error:', error))
   }
 
   // Delete a channel's credentials/password
@@ -48,6 +54,16 @@ export class DeleteChannelService {
       .doc(channelName)
       .collection('credentials')
       .doc('password')
+      .delete();
+  }
+
+  // Delete a channel's users list
+  onDeleteChannelUsers(channelName: string): Promise<any> {
+    return this.firestore
+      .collection('channels')
+      .doc(channelName)
+      .collection('users')
+      .doc('users')
       .delete();
   }
 }
